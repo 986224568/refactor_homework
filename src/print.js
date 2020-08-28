ae03e52a8669b3ecb3ffdddc2ed421e80848afd1
Domingo <986224568@qq.com>
2020-08-28 21:38:20 +0800

    [refactor_homework][Domingo] refactor getOutStanding in print.js

--- src/print.js
+++ src/print.js
@@ -1,17 +1,22 @@
 function printOwing (invoice) {
 
   let result = '***********************\n**** Customer Owes ****\n***********************\n'
-  // calculate outstanding
-  let outstanding = 0;
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
+
+  let outstanding = getOutStanding(invoice);
   invoice.dueDate = getDueDate();
 
   result += `name: ${invoice.customer}\n`;
   result += `amount: ${outstanding}\n`;
   result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
   return result;
+}
+
+function getOutStanding(invoice) {
+  let outstanding = 0;
+  for (const o of invoice.borderSpacing) {
+    outstanding += o.amount;
+  }
+  return outstanding;
 }
 
 function getDueDate() {
