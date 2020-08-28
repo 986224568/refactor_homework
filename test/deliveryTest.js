6ce3874e4e84067247db18988ea06436639864bc
Domingo <986224568@qq.com>
2020-08-28 20:57:19 +0800

    [refactor_homework][Domingo] test add deliveryTest.js

---
+++ test/deliveryTest.js
@@ -0,0 +1,23 @@
+const deliveryTest = require('ava')
+const {deliveryDate} = require('../src/delivery');
+deliveryTest('Should return date when deliveryDate given MA state and rush', t => {
+    const anOrder = {
+        "deliveryState": 'MA',
+        "placedOn":{
+            "plusDays": function (plusDays) {
+                let today = new Date();
+                let plusedDay = new Date();
+                plusedDay.setDate(today.getDate() + plusDays);
+                return plusedDay.toLocaleDateString();
+            }
+        },
+        isRush: true
+    }
+
+    let today = new Date();
+    let plusDay = new Date();
+    plusDay.setDate(today.getDate() + 2)
+
+    t.is(deliveryDate(anOrder),plusDay.toLocaleDateString())
+})
+
