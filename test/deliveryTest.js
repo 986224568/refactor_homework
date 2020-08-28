4c706e510ff35f85c20cac1fb34d4bb2b68634c6
Domingo <986224568@qq.com>
2020-08-28 22:02:06 +0800

    [refactor_homework][Domingo] refactor deliveryTest.js test1 MA true

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -1,25 +1,17 @@
-const deliveryTest = require('ava')
-const {deliveryDate} = require('../src/delivery');
-// deliveryTest('Should return date when deliveryDate given MA state and rush', t => {
-//     const anOrder = {
-//         "deliveryState": 'MA',
-//         "placedOn":{
-//             "plusDays": function (plusDays) {
-//                 let today = new Date();
-//                 let plusedDay = new Date();
-//                 plusedDay.setDate(today.getDate() + plusDays);
-//                 return plusedDay.toLocaleDateString();
-//             }
-//         },
-//         isRush: true
-//     }
+const test = require('ava');
+const {deliveryDate} = require('../src/delivery')
 
-//     let today = new Date();
-//     let plusDay = new Date();
-//     plusDay.setDate(today.getDate() + 2)
+const anOrder ={
+    'deliveryState':'',
+    placedOn: {}
+};
+anOrder.placedOn.plusDays = function (number) {
+    return number;
+};
 
-//     t.is(deliveryDate(anOrder),plusDay.toLocaleDateString())
-// })
-deliveryTest('test', t => {
-    t.is(true, true)
-})
+test('test1 MA true', t=>{
+    anOrder.deliveryState ='MA';
+    let number = deliveryDate(anOrder,true);
+    t.is(2,number);
+});
+
