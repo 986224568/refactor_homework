455ea1fbd7add9ca1aa26a65f274ca110ec92d99
Domingo <986224568@qq.com>
2020-08-28 21:36:59 +0800

    [refactor_homework][Domingo] refactor getDueDate in print.js

--- src/print.js
+++ src/print.js
@@ -1,23 +1,24 @@
 function printOwing (invoice) {
-  let outstanding = 0;
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
 
+  let result = '***********************\n**** Customer Owes ****\n***********************\n'
   // calculate outstanding
+  let outstanding = 0;
   for (const o of invoice.borderSpacing) {
     outstanding += o.amount;
   }
+  invoice.dueDate = getDueDate();
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+  result += `name: ${invoice.customer}\n`;
+  result += `amount: ${outstanding}\n`;
+  result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
+  return result;
 }
+
+function getDueDate() {
+  const today = new Date();
+  return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
 module.exports = {
   printOwing,
 };
