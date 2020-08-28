5deb9d5fc5ee5a3f29dfda9987999d77b2a802af
Domingo <986224568@qq.com>
2020-08-28 20:57:58 +0800

    [refactor_homework][Domingo] test Should return date when deliveryDate given NH state and rush

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -21,3 +21,23 @@ deliveryTest('Should return date when deliveryDate given MA state and rush', t =
     t.is(deliveryDate(anOrder),plusDay.toLocaleDateString())
 })
 
+deliveryTest('Should return date when deliveryDate given NH state and rush', t => {
+    const anOrder = {
+        "deliveryState": 'NH',
+        "placedOn":{
+            "plusDays": function (plusDays) {
+                let today = new Date();
+                let plusedDay = new Date();
+                plusedDay.setDate(today.getDate() + plusDays);
+                return plusedDay.toLocaleDateString();
+            }
+        },
+        isRush: true
+    }
+
+    let today = new Date();
+    let plusDay = new Date();
+    plusDay.setDate(today.getDate() + 3)
+
+    t.is(deliveryDate(anOrder),plusDay.toLocaleDateString())
+})
